4f1fe78a3a81f39240dfd3092fdf1ee5e2e74774
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-08-29 12:24:16 +0300

    Add project files.

---
+++ SchoolProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SchoolInfo schoolInfo = new SchoolInfo(12029, "Kadir", "GÜVEN", 70, 50, 85, "Bahçeşehir Unıversity");
+            schoolInfo.menu();
+            bool cycle = true;
+
+            while (cycle) 
+            {
+                int choose = Convert.ToInt32(Console.ReadLine());
+                schoolInfo.menu();
+                switch (choose)
+                {
+                    case 1:
+                        schoolInfo.getStudentInfo();
+                        break;
+                    case 2:
+                        schoolInfo.getStudentOverall();
+                        break;
+                    case 3:
+                        schoolInfo.getSchoolInfo();
+                        break;
+                    case 4:
+                        Console.WriteLine("Çıkış yaptınız..");
+                        cycle = false;
+                        break;
+                    default:
+                        Console.WriteLine("You have made an invalid transaction..");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ SchoolProject/SchoolInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject
+{
+    public class SchoolInfo
+    {
+        private int studentId;
+        private String studentName;
+        private String studentSurname;
+        private int studentExam_1;
+        private int studentExam_2;
+        private int studentFinal;
+        private int studentOverall;
+        private String schoolName;
+
+        public SchoolInfo(int _studentId, String _studentName, String _studentSurname, int _studentExam_1, int _studentExam_2,
+                          int _studentFinal, String _schoolName)
+        { 
+            studentExam_1 = _studentExam_1; 
+            studentExam_2 = _studentExam_2;
+            studentFinal = _studentFinal;
+            schoolName = _schoolName;
+            studentName = _studentName;
+            studentId = _studentId;
+            studentSurname = _studentSurname;   
+        }
+
+        public void getStudentInfo() 
+        { 
+            Console.WriteLine("Student Information : " + studentId + " " + studentName + " " + studentSurname);
+        }
+
+        public void getStudentOverall() 
+        { 
+            studentOverall = (studentExam_1 + studentExam_2 + studentFinal) / 3;
+
+            Console.WriteLine(" Student Note Overall :" + studentOverall);
+        }
+
+        public void getSchoolInfo() 
+        {
+            Console.WriteLine("School Name : " + schoolName);
+        }
+
+        public void menu() 
+        {
+            Console.WriteLine(" -- Welcome Bahçeşehir Unıversıty -- ");
+            Console.WriteLine("====================================");
+            Console.WriteLine("1 - Get Student Information ");
+            Console.WriteLine("2 - Get Student Note Overall ");
+            Console.WriteLine("3 - Get Scholl Information ");
+            Console.WriteLine("4 - Exit");
+        }
+
+
+    }
+}
